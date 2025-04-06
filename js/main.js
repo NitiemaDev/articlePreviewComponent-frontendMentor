84ba8134992c45cd4331defe54611a0273ff674f
Nitiema Allassane <allassanenitiema@gmail.com>
2025-04-06 16:21:00 +0000

    Ajout du tooltip sur mobile et mise a jour du fichier JS

--- js/main.js
+++ js/main.js
@@ -1,15 +1,46 @@
 const shareButton = document.getElementById('shareIcon');
-const tooltip = document.getElementById('tooltip');
+const tooltipDesktop = document.getElementById('tooltip');
+const tooltipMobile = document.getElementById('tooltip__mobile');
 
-shareButton.addEventListener('click', (event) => {
-  tooltip.classList.toggle("show");
-  shareButton.classList.toggle("active");
 
-});
+// Handle share button
+
+let browserWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+if (browserWidth > 768) {
+  tooltipMobile.classList.remove("displayMobile");
+  shareButton.addEventListener('click', () => {
+    tooltip.classList.toggle("show");
+    shareButton.classList.toggle("active");
+  });
+  
+}
+
+if (browserWidth <= 768) {
+  tooltip.classList.remove("show");
+  shareButton.addEventListener('click', () => {
+    tooltipMobile.classList.toggle("displayMobile");
+  });
+}
+
 
 document.addEventListener('click', (event) => {
-    if (!shareButton.contains(event.target) && !tooltip.contains(event.target)) {
-      tooltip.classList.remove("show");
+    if (!shareButton.contains(event.target) && !tooltipDesktop.contains(event.target) && !tooltipMobile.contains(event.target)) {
+      // Close tooltip if clicking outside of it
+      tooltipMobile.classList.remove("displayMobile");
+      tooltipDesktop.classList.remove("show");
       shareButton.classList.remove("active");
     }
-  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
